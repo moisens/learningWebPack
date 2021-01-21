ed5e007a2db9be3ebb32d68a190d7d3a297e0188
moise <mose.ns@hotmail.com>
2021-01-21 19:03:29 +0100

    running webpack

---
+++ build/bundle.js
@@ -0,0 +1,51 @@
+/******/ (() => { // webpackBootstrap
+/******/ 	var __webpack_modules__ = ([
+/* 0 */,
+/* 1 */
+/***/ ((module) => {
+
+const sum = (a, b) => a + b;
+
+
+module.exports = sum;
+
+
+
+/***/ })
+/******/ 	]);
+/************************************************************************/
+/******/ 	// The module cache
+/******/ 	var __webpack_module_cache__ = {};
+/******/ 	
+/******/ 	// The require function
+/******/ 	function __webpack_require__(moduleId) {
+/******/ 		// Check if module is in cache
+/******/ 		if(__webpack_module_cache__[moduleId]) {
+/******/ 			return __webpack_module_cache__[moduleId].exports;
+/******/ 		}
+/******/ 		// Create a new module (and put it into the cache)
+/******/ 		var module = __webpack_module_cache__[moduleId] = {
+/******/ 			// no module.id needed
+/******/ 			// no module.loaded needed
+/******/ 			exports: {}
+/******/ 		};
+/******/ 	
+/******/ 		// Execute the module function
+/******/ 		__webpack_modules__[moduleId](module, module.exports, __webpack_require__);
+/******/ 	
+/******/ 		// Return the exports of the module
+/******/ 		return module.exports;
+/******/ 	}
+/******/ 	
+/************************************************************************/
+(() => {
+const sum = __webpack_require__(1);
+const total = sum(4,4);
+
+console.log(total);
+
+
+})();
+
+/******/ })()
+;
--- webpack.config.js
+++ webpack.config.js
@@ -5,7 +5,8 @@ const config = {
   output: {
     path: path.resolve(__dirname, 'build'),
     filename: 'bundle.js'
-  }
+  },
+  mode: 'none'
 };
 
 
