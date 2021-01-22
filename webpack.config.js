fcc73f9fc6825da7b120a1fff7c7772aa2e7df7a
moise <mose.ns@hotmail.com>
2021-01-22 17:10:51 +0100

    Intro to loaders Babel setup for es2015 babel configuration

--- webpack.config.js
+++ webpack.config.js
@@ -6,8 +6,27 @@ const config = {
     path: path.resolve(__dirname, 'build'),
     filename: 'bundle.js'
   },
-  mode: 'none'
+  mode: 'none',
+  module: {
+    rules: [
+      {
+        use: 'babel-loader',
+        test: /\.js$/
+      }
+    ]
+  },
 };
 
 
 module.exports = config;
+
+
+
+//Babel-loader : Teaches Babel how to work with webpack
+
+//Babel-core : Knows how to take in code,
+//parse it, and generate some output files.
+
+//Babel-preset-env : Ruleset for telling
+//Babel exactly what pieces of ES2015/6/7  
+//syntax  to look for, and how to turn it into ES5 code
