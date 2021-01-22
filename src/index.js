89d583116747110015c921f3ab989703006e2d13
moise <mose.ns@hotmail.com>
2021-01-22 17:35:20 +0100

    Modules

--- src/index.js
+++ src/index.js
@@ -1,4 +1,7 @@
-const sum = require('./sum');
+//const sum = require('./sum'); //Common js
+import sum from './sum';
+
+
 const total = sum(10,4);
 
 console.log(total);
--- src/sum.js
+++ src/sum.js
@@ -1,5 +1,8 @@
 const sum = (a, b) => a + b;
 
 
-module.exports = sum;
+export default sum;
 
+
+//Commonjs
+//module.exports = sum;
