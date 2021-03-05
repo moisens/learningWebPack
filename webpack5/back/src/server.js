b94e2ce2a2d2e2f7f7d71a00d37d0a07b642811e
moise <mose.ns@hotmail.com>
2021-03-05 16:52:28 +0100

    multiple page webpack

--- webpack5/back/src/server.js
+++ webpack5/back/src/server.js
@@ -6,8 +6,13 @@ const fs = require('fs');
 
 app.use('/static', express.static(path.resolve(__dirname, '../dist')));
 
-app.get('/', (req, res) => {
-  const pathToHtmlFile = path.resolve(__dirname, '../dist/index.html');
+app.get('/hello-world', (req, res) => {
+  const pathToHtmlFile = path.resolve(__dirname, '../dist/hello-world.html');
+  const contentFromHtmlFile = fs.readFileSync(pathToHtmlFile, 'utf-8');
+  res.send(contentFromHtmlFile);
+});
+app.get('/kiwi', (req, res) => {
+  const pathToHtmlFile = path.resolve(__dirname, '../dist/kiwi.html');
   const contentFromHtmlFile = fs.readFileSync(pathToHtmlFile, 'utf-8');
   res.send(contentFromHtmlFile);
 });
